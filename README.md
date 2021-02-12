de0990a996ed8cacc3e8ee43760052b46afc0158
Marco Virgadamo <m.virgadamo@gmail.com>
2021-02-12 17:17:46 +0100

    code efficiency

--- README.md
+++ README.md
@@ -2,13 +2,18 @@
 
 | Variable | Type | Required | Default |
 |:--:|:--:|:--:|:--:|
+| NODE_ENV | String | No | production |
 | TELEGRAM_BOT_TOKEN | String | Yes | - |
 | TELEGRAM_CHAT_ID | String | Yes | - |
 | AMAZON_PRODUCT_IDS | String | Yes | - |
 | AMAZON_BASEURL | String | No | https://www.amazon.it |
-| AMAZON_ACCEPT_LANGUAGE_HEADER | String | No | it-it |
+| AMAZON_HEADER_ACCEPT | String | No | safari standard on mac |
+| AMAZON_HEADER_USERAGENT | String | No | safari standard on mac |
+| AMAZON_HEADER_ACCEPT_LANGUAGE | String | No | it-it |
+| AMAZON_HEADER_ACCEPT_ENCODING | String | No | gzip, deflate, br |
 | AMAZON_MAX_CONCURRENT_REQS | Integer | No | 1 |
 | AMAZON_MIN_MS_BETWEEN_REQS | Integer | No | 333 |
+| CHECK_CRON_ENABLED | Boolean | No | true |
 | CHECK_CRON_PATTERN | String | No | */30 * * * * * |
 | REDIS_HOST | String | No | 127.0.0.1 | 
 | REDIS_PORT | Integer | No | 6379 | 
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const getenv = require('getenv');
 const axios = require('axios');
-const cheerio = require('cheerio');
-const Redis = require("ioredis");
+const { parse } = require('node-html-parser');
+const Redis = require('ioredis');
 const CronJob = require('cron').CronJob;
 const debug = require('debug')('checker');
 const express = require('express');
 const Bottleneck = require('bottleneck');
 const app = express();
 
+const nodeEnv = getenv('NODE_ENV', 'production');
+const checkCronEnabled = getenv.bool('CHECK_CRON_ENABLED', true);
 const checkCronPattern = getenv('CHECK_CRON_PATTERN', '*/30 * * * * *');
 const timeZone = getenv('TZ', 'Europe/Rome');
 
@@ -17,7 +19,12 @@ const amazonLimiter = new Bottleneck({
   maxConcurrent: getenv.int('AMAZON_MAX_CONCURRENT_REQS', 1),
   minTime: getenv.int('AMAZON_MIN_MS_BETWEEN_REQS', 333)
 });
-const amazonLanguageHeader = getenv('AMAZON_ACCEPT_LANGUAGE_HEADER', 'it-it');
+const headers = {
+  'Accept': getenv('AMAZON_HEADER_ACCEPT', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+  'User-Agent': getenv('AMAZON_HEADER_USERAGENT', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'),
+  'Accept-Language': getenv('AMAZON_HEADER_ACCEPT_LANGUAGE', 'it-it'),
+  'Accept-Encoding': getenv('AMAZON_HEADER_ACCEPT_ENCODING', 'gzip, deflate, br')
+}
 
 const telegramBotToken = getenv('TELEGRAM_BOT_TOKEN');
 const telegramChatId = getenv('TELEGRAM_CHAT_ID');
@@ -30,52 +37,41 @@ const redis = new Redis({
   db: getenv.int('REDIS_DB', 0)
 });
 
-async function notify(docInfo) {
-  debug(`Notifying...`);
-  await axios.request({
-    baseURL: `https://api.telegram.org/bot${telegramBotToken}`,
-    url: '/sendMessage',
+async function notify(amazonData) {
+  debug(`[${amazonData.id}] Notifying...`);
+  await axios.get(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
     params: {
       chat_id: telegramChatId,
-      text: `${docInfo.title} - ${docInfo.availability} - ${amazonBaseUrl}/dp/${docInfo.id}`
+      text: `${amazonData.title} - ${amazonData.availability} - ${amazonBaseUrl}/dp/${amazonData.id}`
     }
   });
-  debug(`Notified.`);
+  debug(`[${amazonData.id}] Notified.`);
   return;
 }
 
 async function getAmazonData(productID) {
-  debug(`[${productID}] Checking product`);
-  const response = await amazonLimiter.schedule(() => axios.request({
-    url: `${amazonBaseUrl}/dp/${productID}`,
-    headers: {
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
-      'Accept-Language': amazonLanguageHeader,
-      'Accept-Encoding': 'gzip, deflate, br'
-    }
-  }));
-  debug(`[${productID}] Obtained HTML`);
-  const $ = cheerio.load(response.data);
-  const docInfo = {
+  debug(`[${productID}] Checking product: ${amazonBaseUrl}/dp/${productID}`);
+  const response = await amazonLimiter.schedule(() => axios.get(`${amazonBaseUrl}/dp/${productID}`, { headers }));
+  const root = parse(response.data);
+  const amazonData = {
     id: productID,
-    title: $('#productTitle').text().trim(),
-    availability: $('#availability span').text().trim()
+    title: root.querySelector('#productTitle').rawText.trim(),
+    availability: root.querySelector('#availability').querySelector('span').firstChild.rawText.trim()
   };
-  debug(`[${productID}] Parsed HTML`);
-  return docInfo;
+  debug(`[${productID}] Parsed HTML.`);
+  return amazonData;
 }
 
 async function checkProduct(productID) {
   try {
-    const [docData, docInfo] = await Promise.all([redis.hgetall(productID), getAmazonData(productID)]);
-    debug(`Doc from Redis: ${JSON.stringify(docData)}`);
-    debug(`Doc from Amazon: ${JSON.stringify(docInfo)}`);
-    if (!docData || (docData && docInfo && docInfo.availability && docData.availability !== docInfo.availability)) {
-      debug(`[${productID}] Non existent doc or mismatch`);
-      await redis.hmset(productID, docInfo);
-      await notify(docInfo);
-    } else { debug(`[${productID}] Noting changed, nothing to do`); }
+    const [redisData, amazonData] = await Promise.all([redis.hgetall(productID), getAmazonData(productID)]);
+    debug(`[${productID}] Availability from Redis: ${redisData.availability}`);
+    debug(`[${productID}] Availability from Amazon: ${amazonData.availability}`);
+    if (!redisData || (redisData && amazonData && amazonData.availability && redisData.availability !== amazonData.availability)) {
+      debug(`[${productID}] Non existent doc or mismatch.`);
+      await redis.hmset(productID, amazonData);
+      await notify(amazonData);
+    } else { debug(`[${productID}] Noting changed, nothing to do.`); }
   } catch (error) { debug(error); }
   return;
 }
@@ -83,18 +79,25 @@ async function checkProduct(productID) {
 async function routineFunction() { return Promise.all(amazonProductIDs.map(checkProduct)); }
 
 app.get('/', async (req, res, next) => {
-  const data = [];
-  let el;
-  for (let productID of amazonProductIDs) {
-    el = await redis.hgetall(productID);
-    data.push(el);
-  }
+  const data = await Promise.all(amazonProductIDs.map((id) => redis.hgetall(id)))
   return res.json(data);
 });
 
-const job = new CronJob(checkCronPattern, routineFunction, null, true, timeZone);
-job.start();
+app.post('/check', async (req, res, next) => {
+  res.sendStatus(200);
+  await routineFunction();
+  return;
+})
+
+if (checkCronEnabled) {
+  const job = new CronJob(checkCronPattern, routineFunction, null, true, timeZone);
+  job.start();
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log('App listenting on port ' + port);
+
+if (nodeEnv === 'development') {
+  routineFunction();
+}
